5fb3dcea4e2565e46d24021c9d9b0a9ce9b598c1
riteshalone04 <riteshalone04@gmail.com>
2026-06-22 23:30:27 +0530

    asdfgh

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, get, set } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
 const display = document.getElementById('display');
 const btnContainer = document.getElementById('btns');
 
-// Defined buttons with types for styling
+const firebaseConfig = {
+  apiKey: "AIzaSyDCyZeIVYSJLIK2hAkwGFqubW55UTXunEY",
+  authDomain: "fir-console-b2489.firebaseapp.com",
+  projectId: "fir-console-b2489",
+  storageBucket: "fir-console-b2489.firebasestorage.app",
+  messagingSenderId: "266803189292",
+  appId: "1:266803189292:web:20ddec1bf77d044410e389",
+  measurementId: "G-JP4H9D245R"
+};
+
+const app = initializeApp(firebaseConfig);
+// पुराना कोड: const db = getDatabase(app);
+// नया कोड: इसे ऐसे लिखें
+// यह वाला URL कॉपी करें:
+const db = getDatabase(app, "https://fir-console-b2489-default-rtdb.asia-southeast1.firebasedatabase.app");
+// बटन्स रेंडर करने का कोड
 const buttonConfig = [
     {v:'AC', t:'top'}, {v:'DEL', t:'top'}, {v:'%', t:'top'}, {v:'/', t:'op'},
     {v:'7', t:'num'}, {v:'8', t:'num'}, {v:'9', t:'num'}, {v:'*', t:'op'},
@@ -24,3 +42,14 @@ function handleInput(val) {
     else if(val === '=') { try { display.value = eval(display.value); } catch { display.value = 'Error'; } }
     else display.value = display.value === '0' ? val : display.value + val;
 }
+
+// Firebase Visitor Logic
+const visitsRef = ref(db, 'visitorCount');
+get(visitsRef).then((snapshot) => {
+  let count = snapshot.exists() ? snapshot.val() : 0;
+  count++;
+  set(visitsRef, count);
+  // यह लाइन संख्या को आपकी वेबसाइट पर दिखा देगी
+document.getElementById('visitor-count').innerText = "Total Visitors: " + count;
+  console.log("Visitor number is: " + count);
+});
